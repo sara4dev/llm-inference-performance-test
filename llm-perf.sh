bc39786a27439254087b2a2906082138b077e055
Saravana Periyasamy <saperiyasamy@nvidia.com>
2024-06-13 15:20:52 +0530

    added llm-perf setup script

---
+++ llm-perf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/ray-project/llmperf.git
+cd llmperf
+pip install -e .
+
+export OPENAI_API_BASE="http://localhost:8000/v1"
+export OPENAI_API_KEY="test"
+
+python token_benchmark_ray.py \
+--model "meta-llama/Llama-2-7b-chat-hf" \
+--mean-input-tokens 550 \
+--stddev-input-tokens 150 \
+--mean-output-tokens 150 \
+--stddev-output-tokens 10 \
+--max-num-completed-requests 2 \
+--timeout 600 \
+--num-concurrent-requests 1 \
+--results-dir "result_outputs" \
+--llm-api openai \
+--additional-sampling-params '{}' \
+--results-dir "result_outputs"
